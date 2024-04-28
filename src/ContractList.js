c43966a20ac53b52f3ccf3d557efa7a213cc47ab
Catur Purbo Yuwono <catur.purbo@fit.id>
2024-04-28 10:07:57 +0700

    complete contract list deliverables

--- src/ContractList.js
+++ src/ContractList.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 const ContractList = ({ contracts }) => {
   return (
-    <div>
+    <div className="contract-list">
       <h2>Contracts</h2>
       <ul>
         {contracts.map((contract) => (
           <li key={contract.id}>
             <h3>{contract.name}</h3>
-            {/* You can display additional contract details here, like deliverables */}
+            <h4>Deliverables</h4>
+            <ul>
+              {contract.deliverables.map((deliverable) => (
+                <li key={deliverable.id}>
+                  {deliverable.description} - Due Date: {deliverable.targetDate}
+                </li>
+              ))}
+            </ul>
           </li>
         ))}
       </ul>
